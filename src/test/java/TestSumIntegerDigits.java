c9d409428606b6e3b16d2a3a022edc8e95902770
david.mumladze <dmuml10@gmail.com>
2018-10-23 13:15:54 +0300

    Added solution for SumIntegerDigits

---
+++ src/test/java/TestSumIntegerDigits.java
@@ -0,0 +1,18 @@
+import algorithms.implementation.SumIntegerDigits;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestSumIntegerDigits {
+
+    @Test
+    public void test1() {
+        String a = "111";
+        String b = "2";
+        String x = "01203";
+        assertEquals(3, SumIntegerDigits.sumIntegerDigits(a));
+        assertEquals(2, SumIntegerDigits.sumIntegerDigits(b));
+        assertEquals(6, SumIntegerDigits.sumIntegerDigits(x));
+    }
+
+}
