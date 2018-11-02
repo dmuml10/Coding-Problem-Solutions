01ad2324a1232fef6c19d3dcf2b84b6481220c31
david <dmuml10@gmail.com>
2018-11-02 21:19:42 +0200

    Finished AngryProfessor Solution

---
+++ src/main/java/algorithms/implementation/AngryProfessor.java
@@ -0,0 +1,15 @@
+package algorithms.implementation;
+
+public class AngryProfessor {
+
+    static String angryProfessor(int k, int[] a) {
+        int count = 0;
+        for (int i=0; i<a.length; i++) {
+            if (a[i] > 0) {
+                count ++;
+            }
+        }
+        return a.length - count >= k ? "NO" : "YES";
+    }
+
+}
