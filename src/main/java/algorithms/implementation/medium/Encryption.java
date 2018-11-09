83043b2a55a9a14916050c9401f5055923f88d7e
david.mumladze <dmuml10@gmail.com>
2018-11-09 13:20:32 +0200

    Finished Encryption solution

--- src/main/java/algorithms/implementation/medium/Encryption.java
+++ src/main/java/algorithms/implementation/medium/Encryption.java
@@ -9,16 +9,38 @@ public class Encryption {
         int rowNum = (int)Math.floor(root);
         int colNum = (int)Math.ceil(root);
 
-        String result = "";
+        if (rowNum * colNum < text.length()) {
+            rowNum++;
+        }
+
+        Character[][] array = new Character[rowNum][colNum];
         int index = 0;
+        for (int i=0; i<rowNum; i++) {
+            for (int j=0; j<colNum; j++) {
+
+                if (index < text.length()) {
+                    array[i][j] = text.charAt(index);
+                }
+                index++;
+            }
+        }
+
+        String result = "";
         for (int i=0; i<colNum; i++) {
             for (int j=0; j<rowNum; j++) {
-                result +=text.charAt(index);
-                index++;
+                if (array[j][i] != null) {
+                    result += array[j][i];
+                }
             }
             result += " ";
         }
-        return result;
 
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(encryption("haveaniceday"));
+        System.out.println(encryption("feedthedog"));
+        System.out.println(encryption("chillout"));
     }
 }
