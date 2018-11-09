da14f171a61a3b5bac27e584706e2500b8a83175
david.mumladze <dmuml10@gmail.com>
2018-11-09 11:11:28 +0200

    Added solution for UtopianTree

---
+++ src/main/java/algorithms/implementation/UtopianTree.java
@@ -0,0 +1,17 @@
+package algorithms.implementation;
+
+public class UtopianTree {
+
+    static int utopianTree(int n) {
+        int result = 1;
+        for (int i=1; i<=n; i++) {
+            if (i % 2 == 1) {
+                result *= 2;
+            } else {
+                result ++;
+            }
+        }
+        return result;
+    }
+
+}
