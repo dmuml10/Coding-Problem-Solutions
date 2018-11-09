16cb5c5a8dd893f4f3c78c94037522e56949890e
david.mumladze <dmuml10@gmail.com>
2018-11-09 10:49:42 +0200

    Added solution for DesignerpdfViewer

---
+++ src/main/java/algorithms/implementation/DesignerPdfViewer.java
@@ -0,0 +1,16 @@
+package algorithms.implementation;
+
+public class DesignerPdfViewer {
+
+    static int designerPdfViewer(int[] h, String word) {
+        int max = 1;
+        for (int i=0; i<word.length(); i++) {
+            if (max < h[(int)word.charAt(i) - 97]) {
+                max = h[(int)word.charAt(i) - 97];
+            }
+        }
+
+        return max * word.length();
+    }
+
+}
