1b78de2c0a917f1ef9a1ded0c9a6af5d0a0f6bb1
david.mumladze <dmuml10@gmail.com>
2018-11-12 16:29:00 +0200

    Finished 3DSurfaceArea solution

---
+++ src/main/java/algorithms/implementation/medium/ThreeDSurfaceArea.java
@@ -0,0 +1,28 @@
+package algorithms.implementation.medium;
+
+public class ThreeDSurfaceArea {
+
+  static int surfaceArea(int[][] A) {
+    int area = 0;
+    for (int i=0; i<A.length; i++) {
+      for (int j=0; j<A[0].length; j++) {
+        int height = A[i][j];
+        area += 2;
+        area += getNeighbourHeight(A, i - 1, j, height);
+        area += getNeighbourHeight(A, i + 1, j, height);
+        area += getNeighbourHeight(A, i, j + 1, height);
+        area += getNeighbourHeight(A, i, j - 1, height);
+      }
+    }
+    return area;
+  }
+
+  static int getNeighbourHeight(int[][] A,int i,int j, int originalHeight) {
+    if (i >=0 && i <A.length && j >=0 && j<A[0].length) {
+      return A[i][j] > originalHeight ? 0 : originalHeight - A[i][j];
+    }
+
+    return originalHeight;
+  }
+
+}
