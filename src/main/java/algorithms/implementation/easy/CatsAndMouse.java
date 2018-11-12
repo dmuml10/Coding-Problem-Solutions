2aeba1ed51313936ccd9ba86607b7bccdb8f8980
david.mumladze <dmuml10@gmail.com>
2018-11-12 15:51:52 +0200

    Finished CatsAndMouse solution

---
+++ src/main/java/algorithms/implementation/easy/CatsAndMouse.java
@@ -0,0 +1,17 @@
+package algorithms.implementation.easy;
+
+public class CatsAndMouse {
+
+  static String catAndMouse(int x, int y, int z) {
+    int a = Math.abs(z - x);
+    int b = Math.abs(z - y);
+    if (a < b) {
+      return "Cat A";
+    } else if (a > b) {
+      return "Cat B";
+    } else {
+      return "Mouse C";
+    }
+  }
+
+}
