61e56117a7cec004694907553cbd0fc0cc1dafec
david.mumladze <dmuml10@gmail.com>
2018-11-15 16:20:01 +0200

    Finished SherlockAndTheValidString solution

--- src/main/java/algorithms/implementation/medium/SherlockAndTheValidString.java
+++ src/main/java/algorithms/implementation/medium/SherlockAndTheValidString.java
@@ -16,20 +16,27 @@ public class SherlockAndTheValidString {
 
     Map<Integer, Integer> data = new HashMap<>();
     for (Character key : map.keySet()) {
-        if (data.containsKey(map.get(key))) {
-          data.put(map.get(key), data.get(map.get(key)) + 1);
-        } else {
-          data.put(map.get(key), 1);
-        }
+      if (data.containsKey(map.get(key))) {
+        data.put(map.get(key), data.get(map.get(key)) + 1);
+      } else {
+        data.put(map.get(key), 1);
+      }
     }
 
-    if (data.keySet().size() > 2 || data.keySet().size() < 1) {
+    if (data.keySet().size() > 2) {
       return "NO";
-    } else if (data.keySet().size() == 2) {
-      List<Integer> list = new ArrayList<>(data.values());
-      if (!(list.get(0) == 1 || list.get(1) == 1)) {
+    }
+
+    if (data.keySet().size() == 2) {
+      List<Integer> keys = new ArrayList<>(data.keySet());
+      List<Integer> values = new ArrayList<>(data.values());
+      if (Math.abs(keys.get(0) - keys.get(1)) > 1 && keys.get(0) != 1 && keys.get(1) != 0) {
         return "NO";
       }
+      if (values.get(0) > 1 && values.get(1) > 1) {
+        return "NO";
+      }
+
     }
 
     return "YES";
