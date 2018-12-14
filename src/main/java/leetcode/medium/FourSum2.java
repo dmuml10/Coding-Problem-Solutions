b79530f7879f100aa3fe2efe3bd7ebd7551691ca
david.mumladze <dmuml10@gmail.com>
2018-12-14 15:15:39 +0200

    Finished 4Sum II solution

---
+++ src/main/java/leetcode/medium/FourSum2.java
@@ -0,0 +1,33 @@
+package leetcode.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FourSum2 {
+
+  public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+    int count=0;
+    Map<Integer,Integer> map=new HashMap<>();
+    for(int i=0; i<A.length; i++){
+      for(int j=0; j<B.length; j++){
+        int sum = A[i]+B[j];
+        if(map.containsKey(sum)){
+          map.put(sum,map.get(sum)+1);
+        }
+        else {
+          map.put(sum,1);
+        }
+      }
+    }
+    for(int i=0; i<C.length; i++){
+      for(int j=0; j<D.length; j++){
+        int sum = -C[i]-D[j];
+        if(map.containsKey(sum) && map.get(sum) > 0){
+          count += map.get(sum);
+        }
+      }
+    }
+    return count;
+  }
+
+}
