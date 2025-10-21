18d041f4dbf15bbf0ed23d13a3c10fc466e5c7f0
dmumladze <dmuml10@gmail.com>
2025-10-21 17:34:28 +0400

    Add container with most water

---
+++ src/main/java/leetcode/medium/ContainerWithMostWater.java
@@ -0,0 +1,22 @@
+package leetcode.medium;
+
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        while (left < right) {
+            int currentHeight = Math.min(height[left], height[right]);
+            int currentWidth = right - left;
+            int area = currentHeight * currentWidth;
+            maxArea = Math.max(maxArea, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
