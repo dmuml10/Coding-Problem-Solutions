b001cafee619c9ecb2835875a44d6977c8bca97a
dmumladze <dmuml10@gmail.com>
2026-01-24 14:42:03 +0400

    Add solution

---
+++ src/main/java/leetcode/medium/BestTimeToBuyAndSellStock2.java
@@ -0,0 +1,15 @@
+package leetcode.medium;
+
+public class BestTimeToBuyAndSellStock2 {
+
+    public int maxProfit(int[] prices) {
+        int result = 0;
+        for (int i=0; i<prices.length-1; i++) {
+            if (prices[i]<prices[i+1]) {
+                result += prices[i+1] - prices[i];
+            }
+        }
+        return result;
+    }
+
+}
